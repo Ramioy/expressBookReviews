461f35f366daf4a02cf85259aed8dbdc73676e15
Yoimar Moreno <yoimar.mb@outlook.com>
2024-12-22 15:33:04 -0500

    first part

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,24 +6,77 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  return users.includes((user) => user["username"] == username)
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password)=>{ //returns boolean
+  return !users.includes(user => user["username"] === username && user["password"] === password)
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+        data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+        accessToken, username
+    }
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const isbn = req.params.isbn;
+  const user = req.session.authorization.username;
+  const review = req.body.review;
+  const newReview = true; // Controla si el usuario ha hecho una review sobre ese book
+
+  //Validaciones
+  if(isNaN(isbn)) return res.status(400).send('<p>isbn must to be a number</p>');
+  if(!review) return res.status(400).send('<p>Please provide a valid review.</p>');
+
+
+  // Existe el book con ese isbn
+  const isbn_book = Object.entries(books).find(book => book[0] == isbn)
+  if(!isbn_book) { 
+    return res.status(404).send('<p>We do not have the book you are looking for. Sorry</p>')
+  }
+  if(Object.keys(isbn_book[1]["reviews"]).includes(user)) newReview = false;
+
+  books[isbn]["reviews"][user] = review
+
+  return res.send(`${
+    newReview ? `<p>You have made a new review for the book ${isbn_book[1]["title"]}</p>`
+    : `'<p>Your previous review for the book ${isbn_book[1]["title"]} has been updated succsesfully</p>}'`}`);
+
+})
+
+//Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const user = req.session.authorization.username;
+  const isbn = req.params.isbn;
+  if(isNaN(isbn)) return res.status(400).send('<p>isbn must to be a number</p>');
+
+  const isbn_book = Object.entries(books).find(book => book[0] == isbn)
+  if(!isbn_book) { 
+    return res.status(404).send('<p>We do not have the book you are looking for. Sorry</p>')
+  }
+
+  // El usuario tiene una review en este libro?
+  if(!Object.keys(isbn_book[1]["reviews"]).includes(user)) {
+    return res.status(404).send("<p>You haven't made any review for this book yet.</p>")
+  }
+
+  delete books[isbn]["reviews"][user]
+  return res.status(200).send("Your review has been deleted from this book");
+
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,89 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+        if (!isValid(username)) { // verificamos que el usuario no exista
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Averiguamos si hay libros disponibles
+  if (Object.keys(books).length !== 0) {
+    return res.send(JSON.stringify(books)) // regresa los books
+  }
+  // Si no hay books, entonces envía un mensaje acorde con código 404 NOT FOUND
+  return res.status(404).send('<p>We have no books in this shop</p>')
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = parseInt(req.params.isbn) // ISBN es un número (las keys del objeto books)
+  if(isNaN(isbn)) { // verificamos si el param isbn no se puede parsear como number
+    return res.status(400).send('<p>isbn must to be a number</p>') // return 400 BAD REQUEST
+  }
+  // verificamos si el objeto books tiene un book con la key igual al isbn 
+  const isbn_book = Object.entries(books).find(book => book[0] == isbn)
+  if(!isbn_book) { // Si la key (aka ISBN) no existe, entonces enviamos un 404 NOT FOUND
+    return res.status(404).send('<p>We do not have the book you are looking for. Sorry</p>')
+  }
+  // Finalmente, retornamos el book
+  return res.send(JSON.stringify(isbn_book))
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = "" + req.params.author
+  if (author === ':author' || !author.trim()) { // verificamos que author no sea una cadena vacía
+    return res.status(400).send('<p>Please, provide a valid author name</p>') // return 400 BAD REQUEST
+  }
+  // Verificamos si existe un libro cuyo autor sea el param recibido
+  const author_book = Object.entries(books).find(book => book[1]["author"] === author)
+  if(!author_book) {
+    return res.status(404).send('<p>We do not have the book you are looking for. Sorry</p>')
+  }
+  // Retornamos el book
+  return res.send(JSON.stringify(author_book))
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = "" + req.params.title
+  if (title === ':title' || !title.trim()) { // verificamos que title no sea una cadena vacía
+    return res.status(400).send('<p>Please, provide a valid book title</p>') // return 400 BAD REQUEST
+  }
+  // Verificamos si existe un libro cuyo title sea el param recibido
+  const title_book = Object.entries(books).find(book => book[1]["title"] === title)
+  if(!title_book) {
+    return res.status(404).send('<p>We do not have the book you are looking for. Sorry</p>')
+  }
+  // Retornamos el book
+  return res.send(JSON.stringify(title_book))
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = parseInt(req.params.isbn)
+  if(isNaN(isbn)) { 
+    return res.status(400).send('<p>isbn must to be a number</p>')
+  }
+  // verificamos si el objeto books tiene un book con la key igual al isbn 
+  const isbn_book = Object.entries(books).find(book => book[0] == isbn)
+  if(!isbn_book) {
+    return res.status(404).send('<p>We do not have the book you are looking for. Sorry</p>')
+  }
+  // Finalmente, retornamos el book con el formato siguiente:
+  return res.send(`<h2>Reviwes for the book: ${isbn_book[1]["title"]}</h2> ${
+    !Object.keys(isbn_book[1]["reviews"]).length ? "<p>This book has no reviews yet...</p>"
+    : Object.entries(isbn_book[1]["reviews"])}`)
 });
 
 module.exports.general = public_users;
